c75151e5ecc8fc08fea69e9fe21acc8ca355d8de
Lilia0leynikova <Liliya.nik.na@gmail.com>
2022-01-29 19:11:06 +0200

    create: added modal section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -23,5 +24,27 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
   console.log("Клик по кнопке меню");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle("navbar-bottom--visible");
+});
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+}
 });
